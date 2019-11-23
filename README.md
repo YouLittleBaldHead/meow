5bd97c7490efad7bfd4a6e9ab3868813fb8ec967
miaotony <41962043+miaotony@users.noreply.github.com>
2019-11-24 01:35:08 +0800

    Clone V0.11.0.20191121

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # meow
-Hackathon 2019 East China
+**Hackathon 2019 East China**  
+
+**看什么看你个小秃头！**
---
+++ settings.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+settings  相关设置
+
+@Author: MiaoTony
+"""
+
+DEBUG = False
+
+VERSION = "V0.11.0.20191121"
